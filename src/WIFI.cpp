51f7fbb1bba75fcdffd60d3c84ea0f0e2b8822fc
Ramkh05ho <rkhosho@cps.edu>
2023-06-29 09:52:55 -0500

    WIFI

---
+++ src/WIFI.cpp
@@ -0,0 +1,21 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/Users/ramikhosho/WIFI/src/WIFI.ino"
+void setup();
+void loop();
+#line 1 "/Users/ramikhosho/WIFI/src/WIFI.ino"
+SYSTEM_THREAD(ENABLED);
+
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
---
+++ src/WIFI.ino
@@ -0,0 +1,12 @@
+SYSTEM_THREAD(ENABLED);
+
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
